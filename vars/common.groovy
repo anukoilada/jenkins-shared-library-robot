15ff12eafd9b5e33f17a08aa996ba46ad0f15b41
anupama <anupama@gmail.com>
2023-04-12 23:10:45 -0400

    Git Auto Push

--- vars/common.groovy
+++ vars/common.groovy
@@ -1,6 +1,6 @@
 def sonarChecks() {
     stage('Sonar Checks') {
-        //  sh "sonar-scanner -Dsonar.host.url=http://172.31.9.236:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
+        //  sh "sonar-scanner -Dsonar.host.url=http://172.31.0.157:9000 ${ARGS} -Dsonar.projectKey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}"
         //  sh "curl https://gitlab.com/thecloudcareers/opensource/-/raw/master/lab-tools/sonar-scanner/quality-gate > quality-gate.sh" 
         //  sh "bash -x quality-gate.sh ${SONAR_USR} ${SONAR_PSW} ${SONAR_URL} ${COMPONENT}"
           sh "echo Starting Code Quality Analysis"
