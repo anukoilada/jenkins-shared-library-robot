666d041d81a118bb7659b37caa092cd670acf01c
anupama <anupama@gmail.com>
2023-04-15 19:45:23 -0400

    Git Auto Push

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,6 +1,6 @@
 def call() {
     node {
-        git branch: 'main', url: "https://github.com/b53-clouddevops/${COMPONENT}.git"
+        git branch: 'main', url: "https://github.com/anukoilada/${COMPONENT}.git"
         env.APP_TYPE = "maven" 
         common.lintChecks()
         env.ARGS="-Dsonar.java.binaries=target/"
